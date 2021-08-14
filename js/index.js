534bb79d68fc47e42846d0142a6f5faf67608507
Khanh <Khanh13Nguyen13@gmail.com>
2021-08-14 13:10:24 +0300

    Init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { InputFiles } from './ClassInputFiles.js';
+
+// class InputFiles {
+//     constructor(selector) {
+//         this.selector = selector;
+//     }
+
+//     renderFiles() {
+//         console.log(this.selector);
+//     }
+// }
+
+let files = new InputFiles('#inputFiles');
